5489a98e826209296f6de02c2c4f07ba72d8789c
Rajetha <57398800+Rajetha@users.noreply.github.com>
2020-01-16 12:40:22 +0530

    Add files via upload

---
+++ string.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo strlen("hello");
+    echo "<br>";
+    echo str_word_count("heelo world");
+    echo "<br>";
+    echo strrev("hrllo");
+    echo "<br>";
+    echo strpos("hello world","world");
+    echo "<br>";
+    echo str_replace("world","hi","hello world");
+    ?>
+</body>
+</html>
